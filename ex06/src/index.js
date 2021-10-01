e5a839df9985e3e27ccc2282eb600b5aa60b444e
ElmaSakic1997 <elma.sakic97@gmail.com>
2021-10-01 16:38:12 +0200

    JSX: JavaScript + XML

---
+++ ex06/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends React.Component {
+  render() {
+    // Change code below this line
+    const world = "World";
+    // Change code above this line
+    return (
+      <div className="App">
+        <h1>Hello, {world}!</h1> 
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
